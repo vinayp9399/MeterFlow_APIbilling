aacd53d761ef2af48c88274f60cb5d3538d121e7
Vinay Phatarpekar <vinayp9399@gmail.com>
2026-05-02 15:47:47 +0530

    changes done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://meterflow-apibilling.onrender.com',
         changeOrigin: true,
       },
       '/gateway': {
-        target: 'http://localhost:5000',
+        target: 'https://meterflow-apibilling.onrender.com',
         changeOrigin: true,
       },
     },
